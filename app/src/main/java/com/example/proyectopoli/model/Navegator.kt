c0c626bbc1b93254084b212d35825d6f315a767b
CristianCruz1989 <crcruzgu@poligran.edu.co>
2025-04-08 23:40:02 -0500

    avance navegator2

--- app/src/main/java/com/example/proyectopoli/model/Navegator.kt
+++ app/src/main/java/com/example/proyectopoli/model/Navegator.kt
@@ -1,3 +1,24 @@
 package com.example.proyectopoli.model
 
-data class Navegator()
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class Navegator : AppCompatActivity () {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val miVisorWeb: WebView = findViewById(R.id.miviorweb)
+        miVisorWeb.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view : webview?,
+                request: WebResourceRequest?
+            ): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+    }
+
+}
